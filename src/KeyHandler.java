350fdc3d5ec2ee5db02cc58fab5e3159ade6add9
elsa <elsalin@kth.se>
2023-04-25 09:43:54 +0200

    clean comments

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -6,11 +6,6 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
     boolean left, right;
 
-    Player player;
-    public KeyHandler(){
-    
-    }
-
     @Override
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
@@ -23,14 +18,10 @@ public class KeyHandler implements KeyListener {
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
-            //player.setDirection(-0.1);
             left = true;
-            //player.red = Color.GREEN;
         }
         if (key == KeyEvent.VK_RIGHT) {
-            //player.setDirection(0.1);
             right = true;
-           //player.red = Color.BLUE;
         }
     }
 
