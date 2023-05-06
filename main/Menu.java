b963c273058bede6f46f47ce5a2e09c03fcd84d1
lmark <lmark@kth.se>
2023-05-06 20:43:30 +0200

    Solve #44

--- main/Menu.java
+++ main/Menu.java
@@ -23,16 +23,14 @@ public class Menu implements ActionListener{
     public JPanel pausePanel = new JPanel();
     private JFrame window;
     private Panel panel;
-    private JLayeredPane pane;
-    private Main main;
+    public JComponent startPage;
+    public JComponent loadPage;
 
     private int width, height;
 
     private BufferedImage background;
     
     public Menu(Panel panel, JFrame window, JLayeredPane pane){
-         
-        this.pane = pane;
         this.panel = panel;
         this.window = window;
         width = 32*3*12;
@@ -42,7 +40,11 @@ public class Menu implements ActionListener{
         // main menu
         menuPanel.setPreferredSize(new Dimension(width, height));
         menuPanel.setFocusable(true);
-        menuPanel.add(drawMain());
+        startPage = drawMain();
+        loadPage = new JLayeredPane();
+        menuPanel.add(startPage);
+        menuPanel.add(loadPage);
+        loadPage.setVisible(false);
         menuPanel.setBounds(0, 0, width, height);
 
         // pause menu
@@ -139,7 +141,7 @@ public class Menu implements ActionListener{
 
         // Exit to main menu
         JButton exit = new JButton("MAIN MENU");
-        exit.setActionCommand("exitToMain");
+        exit.setActionCommand("exitGame");
         exit.addActionListener(this);
         exit.setBounds(width/2 - 100, 350, 200, 50);
         
@@ -151,30 +153,91 @@ public class Menu implements ActionListener{
         return layers;
     }
 
+    private JComponent drawLoad(){
+        // Layers
+        JLayeredPane layers = new JLayeredPane();
+        layers.setPreferredSize(new Dimension(width, height));
+
+        // Background image
+        Image dimg = background.getScaledInstance(background.getWidth()*3, background.getHeight()*3, Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(dimg);
+        JLabel bg = new JLabel(icon);
+        bg.setBounds(0, 0, width, height);
+
+        // Load title
+        JLabel title = new JLabel("CHOOSE LOAD", SwingConstants.CENTER);
+        Font titleFont = new Font("Arial", Font.BOLD, 80);
+        title.setForeground(Color.WHITE);
+        title.setFont(titleFont);
+        title.setBounds(width/2 - 300, 50, 600, 150);
+        
+        for (int i = 0; i < panel.playerData.loadCount; i++){
+            JButton newBt = new JButton("LOAD: " + (i + 1));
+            newBt.setActionCommand("loadSave");
+            newBt.addActionListener(this);
+            newBt.setBounds(width/2 - 100, (300 + 50*i), 200, 50);
+            layers.add(newBt, JLayeredPane.POPUP_LAYER);
+        }
+
+        // Exit
+        JButton exit = new JButton("BACK");
+        exit.setActionCommand("exitToMain");
+        exit.addActionListener(this);
+        exit.setBounds(width/2 - 100, 700, 200, 50);
+        
+        layers.add(bg, JLayeredPane.DEFAULT_LAYER);
+        layers.add(title, JLayeredPane.POPUP_LAYER);
+        layers.add(exit, JLayeredPane.POPUP_LAYER);
+        layers.setVisible(true);
+        
+        return layers;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         String actionStr = e.getActionCommand();
         if(actionStr.equals("start")){
             System.out.println("start");
+            panel.playerData.saveNum = panel.playerData.loadCount + 1;
+            panel.playerData.loadCount++;
             menuPanel.setVisible(false);
-            panel.startGame();
+            panel.startNewGame();
         } else if(actionStr.equals("continue")){
             System.out.println("continue");
             pausePanel.setVisible(false);
             panel.startGame();
         } else if(actionStr.equals("load")){
             System.out.println("load");
-            
+            //panel.playerData.loadSaves();
+            loadPage = drawLoad();
+            menuPanel.add(loadPage);
+            startPage.setVisible(false);
+            loadPage.setVisible(true);
         } else if(actionStr.equals("exit")){
             System.out.println("exit");
+            panel.playerData.saveGame();
             window.dispose();
             window.setVisible(false);
             System.exit(0);
-        } else if(actionStr.equals("exitToMain")){
-            System.out.println("exitToMain");
+        } else if(actionStr.equals("exitGame")){
+            System.out.println("exitGame");
             panel.stopGame();
+            panel.playerData.setSave();
             menuPanel.setVisible(true);
             pausePanel.setVisible(false);
+        }else if(actionStr.equals("exitToMain")){
+            System.out.println("exitToMain");
+            loadPage.setVisible(false);
+            startPage.setVisible(true);
+        } else if(actionStr.equals("loadSave")){
+            System.out.println("loadSave");
+            JButton button = (JButton) e.getSource();
+            panel.playerData.saveNum = Integer.parseInt(String.valueOf(button.getText().charAt(6)));
+            panel.playerData.setLoad();
+            menuPanel.setVisible(false);
+            loadPage.setVisible(false);
+            startPage.setVisible(true);
+            panel.startGame();
         }
     }
 }
